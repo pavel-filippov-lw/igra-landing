0c51a9e0de8ea29a985549975f9f3ee2bd3f4244
gusyaraaa <gusyaraaa@yandex.ru>
2025-01-08 15:39:47 +0300

    update video fragment positioning

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react"
 
+import { Flex } from "~/shared/ui"
+
 import { HeroBenefits } from "../HeroBenefits"
 import { HeroContent } from "../HeroContent"
 import heroVideo from './assets/hero.webm'
@@ -8,16 +10,18 @@ import classes from './Hero.module.scss'
 export const Hero: FC = () => {
   return (
     <div className={classes.root}>
-      <HeroContent className={classes.content} />
+      <Flex className={classes.layout}>
+        <HeroContent className={classes.content} />
+        <video
+          autoPlay
+          muted
+          loop
+          className={classes.video}
+        >
+          <source src={heroVideo} type="video/webm" />
+        </video>
+      </Flex>
       <HeroBenefits />
-      <video
-        autoPlay
-        muted
-        loop
-        className={classes.video}
-      >
-        <source src={heroVideo} type="video/webm" />
-      </video>
     </div>
   )
 }
