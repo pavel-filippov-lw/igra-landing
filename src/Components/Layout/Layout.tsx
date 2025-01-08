639d8cfbbdcdc6f71f989209dc09b03e79893932
gusyaraaa <gusyaraaa@yandex.ru>
2025-01-08 16:42:19 +0300

    fix cube

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -6,6 +6,9 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className={classes.root}>
       {children}
+      <div className={classes.copyright}>
+        &copy; 2024-2025 Igra Labs
+      </div>
     </div>
   )
 }
