314d675b02bbccc1660e51ed553af3c174103928
Pavel Emdin <zanziver@gmail.com>
2025-01-22 20:04:00 +0100

    upd

--- src/Components/Team/Team.tsx
+++ src/Components/Team/Team.tsx
@@ -5,6 +5,9 @@ import denPhoto from './assets/den.png'
 import pavelPhoto from './assets/pavel.png'
 import romanPhoto from './assets/rom.png'
 import mikePhoto from './assets/mike.png'
+import ilyaPhoto from './assets/ilya.png'
+import anonPhoto from './assets/anon.png'
+
 import classes from './Team.module.scss'
 
 const teamMembers = [
@@ -16,15 +19,15 @@ const teamMembers = [
     linkedinUrl: 'https://www.linkedin.com/in/emdin/',
   },
   {
-    photoUrl: '',
+    photoUrl: anonPhoto,
     name: ['***** ********', 'Navigator and Boatswain'],
-    role: 'CTO / VP R&D',
+    role: 'CTO, VP R&D',
     description: 'To be disclosed',
     linkedinUrl: '',
   },
   {
     photoUrl: denPhoto,
-    name: ['Denis Mashkevich', 'Supercargo'],
+    name: ['Denis Mashkevich', 'Supercargo and Cabin Boy'],
     role: 'Chief of Strategy, Devops',
     description: 'Founder and engineer with 25 years in software development, 18 years leading teams, ex teamlead @ DAGlabs',
     linkedinUrl: 'https://www.linkedin.com/in/denis-mashkevich-78b6aa/',
@@ -37,21 +40,21 @@ const teamMembers = [
     linkedinUrl: 'https://www.linkedin.com/in/mike-zak-2721324b/',
   },
   {
-    photoUrl: '',
+    photoUrl: ilyaPhoto,
     name: ['Ilya Arosov', 'Sailor'],
     role: 'Software engineer',
     description: 'Ex-Intel, embedded systems',
     linkedinUrl: 'https://www.linkedin.com/in/arosov/',
   },
   {
-    photoUrl: '',
+    photoUrl: anonPhoto,
     name: ['**** ********', 'Sailor'],
     role: 'Software engineer',
     description: 'To be disclosed',
     linkedinUrl: '',
   },
   {
-    photoUrl: '',
+    photoUrl: anonPhoto,
     name: ['****** *****', 'Sailor'],
     role: 'Software engineer',
     description: 'To be disclosed',
