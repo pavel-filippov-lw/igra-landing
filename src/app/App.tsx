2753cd3feca901d8cb16e8c386bf054df12896f8
gusyaraaa <gusyaraaa@yandex.ru>
2025-01-30 22:05:49 +0300

    fix build

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,11 @@ import './fonts/fonts.scss'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 import { FC } from "react"
-import { SkeletonTheme } from 'react-loading-skeleton'
 
 import { AppRouter } from './router'
 
 export const App: FC = () => {
   return (
-    <SkeletonTheme baseColor="#202020" highlightColor="#444">
-      <AppRouter />
-    </SkeletonTheme>
+    <AppRouter />
   )
 }
