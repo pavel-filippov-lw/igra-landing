1db085d2e478b2f14fde1380fc132a381c4dadc5
gusyaraaa <gusyaraaa@yandex.ru>
2025-05-08 14:07:46 +0200

    fix difficulty min value

--- src/Components/Difficulty/Difficulty.tsx
+++ src/Components/Difficulty/Difficulty.tsx
@@ -7,7 +7,11 @@ import classes from './Difficulty.module.scss'
 const LS_KEY = 'difficulty'
 const DEFAULT_DIFFICULTY = 1.45e+18
 
-const formatDifficulty = (value: number): number => +(value / 1e18).toFixed(2)
+const formatDifficulty = (value: number): number => {
+  const difficulty = value / 1e18
+
+  return +(difficulty >= 1 ? difficulty : 1).toFixed(2)
+}
 
 const fetcher = (url: string) => axios.get(url).then(({ data }) => data[0].difficulty)
 
