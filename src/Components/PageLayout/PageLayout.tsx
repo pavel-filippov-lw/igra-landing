0616acdb2a13cd1fb929ed59da8dff8992995296
Dmitry Bekker <gusyaraaa@yandex.ru>
2026-02-08 22:56:16 +0700

    fix hero video

--- src/Components/PageLayout/PageLayout.tsx
+++ src/Components/PageLayout/PageLayout.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx"
 import { FC, PropsWithChildren } from "react"
 
 import { Flex } from "~/shared/ui"
@@ -7,10 +8,12 @@ import { Header } from "../Header"
 import classes from './PageLayout.module.scss'
 
 export const PageLayout: FC<PropsWithChildren> = ({ children }) => {
+  const isSafari = navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome')
+
   return (
     <div className={classes.root}>
       <Header />
-      <div className={classes.radialBg} />
+      <div className={clsx(classes.radialBg, { [classes.isSafari]: isSafari })} />
       <Flex flexDirection='column' className={classes.layout}>
         <div className={classes.content}>
           {children}
