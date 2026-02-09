ffbcaad77309246e608b40c63beff3ff7981fe61
Dmitry Bekker <gusyaraaa@yandex.ru>
2026-02-09 12:04:21 +0700

    minor Safari fix

--- src/Components/PageLayout/PageLayout.tsx
+++ src/Components/PageLayout/PageLayout.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx"
 import { FC, PropsWithChildren } from "react"
 
+import { to } from "~/shared/lib"
 import { Flex } from "~/shared/ui"
 
 import { Footer } from "../Footer"
@@ -8,7 +9,7 @@ import { Header } from "../Header"
 import classes from './PageLayout.module.scss'
 
 export const PageLayout: FC<PropsWithChildren> = ({ children }) => {
-  const isSafari = navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome')
+  const isSafari = navigator.userAgent.includes('Safari') && !navigator.userAgent.includes('Chrome') && location.pathname === to.hero()
 
   return (
     <div className={classes.root}>
